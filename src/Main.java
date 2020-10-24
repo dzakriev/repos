e0eac003a46ce9cb0e22406127e5943db2f49b15
Unknown <dzakriev@inbox.ru>
2020-10-24 14:41:43 +0300

    Created A+B programm

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Jamil");
+        Scanner in = new Scanner(System.in);
+        int a, b;
+        a = in.nextInt();
+        b = in.nextInt();
+        int c = a + b;;
+        System.out.println(c);
     }
 }
